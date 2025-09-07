adc51038ebf6346e09cd515fb9656e3a898d1531
Arya Deep Singh <singharya9693@gmail.com>
2025-09-07 18:48:45 +0530

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,21 +1,15 @@
 "use client";
-
 import { ThirdwebProvider } from "thirdweb/react";
-import { createThirdwebClient } from "thirdweb";
-import { somniaShannon } from "@/lib/somnia";
+import React from "react";
 
 if (!process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID) {
   // eslint-disable-next-line no-console
   console.warn("Missing NEXT_PUBLIC_THIRDWEB_CLIENT_ID in env");
 }
 
-const client = createThirdwebClient({
-  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID!,
-});
-
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThirdwebProvider client={client} activeChain={somniaShannon}>
+    <ThirdwebProvider>
       {children}
     </ThirdwebProvider>
   );
